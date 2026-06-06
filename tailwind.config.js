5312a26223f0966144832e0ca7a2dd985762e9f3
Cave <NomAnor67@protonmail.com>
2026-06-06 10:54:59 -0600

    DEV BUILD v0.1020

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-const tailwindcssAnimate = require("tailwindcss-animate");
+import tailwindcssAnimate from "tailwindcss-animate";
 
-module.exports = {
+export default {
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
